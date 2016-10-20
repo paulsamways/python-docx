d5f1cb95b79e58d68811948e496f9c184ebad8e6
Paul Samways <paul.samways@data61.csiro.au>
2016-10-20 12:06:10 +1100

    noBreakHyphen run elements are replaced with a Non-Breaking Hyphen (U+2011) character

--- docx/oxml/text/run.py
+++ docx/oxml/text/run.py
@@ -96,6 +96,8 @@ class CT_R(BaseOxmlElement):
                 text += '\t'
             elif child.tag in (qn('w:br'), qn('w:cr')):
                 text += '\n'
+            elif child.tag == qn('w:noBreakHyphen'):
+                text += '‑'
         return text
 
     @text.setter
